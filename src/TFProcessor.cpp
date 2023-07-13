d16496b5ec5ce13bff8c78c5e0a7c9208af5dd60
dehan2 <rolika4316@gmail.com>
2023-07-13 15:40:45 +0900

    Frame corrected

--- src/TFProcessor.cpp
+++ src/TFProcessor.cpp
@@ -1,5 +1,5 @@
 #include "TFProcessor.h"
-
+#include <iostream>
 
 TFProcessor::TFProcessor() 
   : m_tfListener(m_tfBuffer)
@@ -16,7 +16,7 @@ void TFProcessor::publish_transformed_message_for_lidar1(const sensor_msgs::Lase
     geometry_msgs::TransformStamped transformStamped;
     try
     {
-      transformStamped = m_tfBuffer.lookupTransform("base_link", "lidar1_link", ros::Time(0));
+      transformStamped = m_tfBuffer.lookupTransform("lidar1_link", "base_link", ros::Time(0));
     }
     catch (tf2::TransformException &ex) 
     {
@@ -28,6 +28,7 @@ void TFProcessor::publish_transformed_message_for_lidar1(const sensor_msgs::Lase
     transformed_msg = *msg;
     transformed_msg.header.seq = msg->header.seq;
     transformed_msg.header.frame_id = transformStamped.header.frame_id;
+
     transformed_msg.header.stamp = msg->header.stamp;
 
     pTFPublisherForLidar1->publish(transformed_msg);
@@ -43,7 +44,7 @@ void TFProcessor::publish_transformed_message_for_lidar2(const sensor_msgs::Lase
     geometry_msgs::TransformStamped transformStamped;
     try
     {
-      transformStamped = m_tfBuffer.lookupTransform("base_link", "lidar2_link", ros::Time(0));
+      transformStamped = m_tfBuffer.lookupTransform("lidar2_link", "base_link", ros::Time(0));
     }
     catch (tf2::TransformException &ex) 
     {
@@ -70,7 +71,7 @@ void TFProcessor::publish_transformed_message_for_camera(const sensor_msgs::Poin
     geometry_msgs::TransformStamped transformStamped;
     try
     {
-      transformStamped = m_tfBuffer.lookupTransform("base_link", "camera_link", ros::Time(0));
+      transformStamped = m_tfBuffer.lookupTransform("camera_link", "base_link", ros::Time(0));
     }
     catch (tf2::TransformException &ex) 
     {
